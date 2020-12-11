542b0db3ac4157fa7e19952ef329c651a01a9507
lisha <lisha@163.com>
2020-12-11 14:08:30 +0800

    update.gitignore

---
+++ src/main/java/cn/itcast/po/User.java
@@ -0,0 +1,11 @@
+package cn.itcast.po;
+
+/**
+ * @author shkstart
+ * @creat 2020-12-11 13:49
+ */
+public class User {
+
+
+
+}
---
+++ src/test/java/cn/itcast/test/UserTest.java
@@ -0,0 +1,8 @@
+package cn.itcast.test;
+
+/**
+ * @author shkstart
+ * @creat 2020-12-11 13:50
+ */
+public class UserTest {
+}
